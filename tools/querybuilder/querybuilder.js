378cb626ca88fcd50e2239b027fac5251c43046a
Laurel Timko <laurel.timko@jmp.com>
2025-03-21 14:57:36 -0400

    Imported jquery querybuilder for plugin

--- tools/querybuilder/querybuilder.js
+++ tools/querybuilder/querybuilder.js
@@ -11,27 +11,7 @@ export async function getProperties() {
   return properties;
 }
 
-export function addForm() {
-  var rules_basic = {
-    condition: 'AND',
-    rules: [{
-      id: 'price',
-      operator: 'less',
-      value: 10.25
-    }, {
-      condition: 'OR',
-      rules: [{
-        id: 'category',
-        operator: 'equal',
-        value: 2
-      }, {
-        id: 'category',
-        operator: 'equal',
-        value: 1
-      }]
-    }]
-  };
-  
+export function addForm() { 
   $('#builder-basic').queryBuilder({
     plugins: ['bt-tooltip-errors'],
     
@@ -80,25 +60,7 @@ export function addForm() {
       validation: {
         format: /^.{4}-.{4}-.{4}$/
       }
-    }],
-  
-    rules: rules_basic
-  });
-  
-  $('#btn-reset').on('click', function() {
-    $('#builder-basic').queryBuilder('reset');
-  });
-  
-  $('#btn-set').on('click', function() {
-    $('#builder-basic').queryBuilder('setRules', rules_basic);
-  });
-  
-  $('#btn-get').on('click', function() {
-    var result = $('#builder-basic').queryBuilder('getRules');
-    
-    if (!$.isEmptyObject(result)) {
-      alert(JSON.stringify(result, null, 2));
-    }
+    }]
   });
 }
 
@@ -112,57 +74,8 @@ async function buildDropdownOptions() {
   });
 }
 
-function typeChange(e) {
-  let hasDate = false;
-  let hasTime = false;
-  switch (e.target.value) {
-    case 'date':
-      hasDate = true;
-      break;
-    case 'time':
-      hasTime = true;
-      break;
-    default:
-      hasDate = true;
-      hasTime = true;
-      break;
-  }
-  document.querySelectorAll('form#picker input[type=date]').forEach((date) => {
-    date.hidden = !hasDate;
-    date.required = hasDate;
-  });
-  document.querySelectorAll('form#picker [name=time-picker]').forEach((time) => {
-    time.hidden = !hasTime;
-    time.required = hasTime;
-  });
-}
-
 export function useButtonClicked(e, actions, doc = document) {
   e.preventDefault();
-
-  const form = doc.querySelector('form#picker');
-  if (!form.reportValidity()) {
-    return;
-  }
-
-  const inputType = doc.querySelector('form#typeselect input[name="date-picker"]:checked').value;
-  let seldate = '';
-  if (inputType.includes('date')) {
-    seldate = doc.querySelector('form#picker input[type=date]').value;
-  }
-
-  let seltime = '';
-  let seltz = '';
-  if (inputType.includes('time')) {
-    seltime = doc.querySelector('form#picker input[type=time]').value;
-    seltz = doc.querySelector('form#picker select#time-zone').value;
-  } else {
-    actions.sendText(seldate);
-    actions.closeLibrary();
-    return;
-  }
-
-  const d = new Date(`${seldate}T${seltime}${seltz}`);
 
   const result = d.toISOString();
   const cIdx = result.lastIndexOf(':');
@@ -174,21 +87,28 @@ export function useButtonClicked(e, actions, doc = document) {
   actions.closeLibrary();
 }
 
-function initControls(actions) {
-  document.querySelectorAll('form#typeselect input[type=radio]').forEach((radio) => {
-    radio.addEventListener('change', typeChange);
+function initControls() {
+  $('#btn-reset').on('click', function(e) {
+    e.preventDefault();
+    $('#builder-basic').queryBuilder('reset');
   });
 
-  document.querySelectorAll('button#use').forEach((button) => {
-    button.onclick = (e) => useButtonClicked(e, actions);
+  $('#btn-get').on('click', function(e) {
+    e.preventDefault();
+    var result = $('#builder-basic').queryBuilder('getRules');
+    console.log(result);
+    
+    if (!$.isEmptyObject(result)) {
+      alert(JSON.stringify(result, null, 2));
+    }
   });
 }
 
 (async function init() {
   console.log('init');
   //const { actions } = await DA_SDK;
-
+  
   buildDropdownOptions();
   addForm();
-  initControls(actions);
+  //initControls(actions);
 }());
