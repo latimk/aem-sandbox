509ecb8529e0ec540dfc54e1a60942e0d3b29189
Laurel Timko <laurel.timko@jmp.com>
2025-03-21 13:45:28 -0400

    Test add querybuilder js

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -10,6 +10,33 @@ async function getQueryIndex(url) {
   return null;
 }
 
+/**
+ * From Adobe blog: https://github.com/adobe/blog/
+ * Create an element with ID, class, children, and attributes
+ * @param {String} tag the tag nav of the element
+ * @param {Object} attributes the attributes of the tag
+ * @param {HTMLElement} html the content of the element
+ * @returns {HTMLElement} the element created
+ */
+function createTag(tag, attributes, html) {
+  const el = document.createElement(tag);
+  if (html) {
+    if (html instanceof HTMLElement) {
+      el.append(html);
+    } else {
+      el.insertAdjacentHTML('beforeend', html);
+    }
+  }
+  if (attributes) {
+    Object.keys(attributes).forEach((key) => {
+      el.setAttribute(key, attributes[key]);
+    });
+  }
+  return el;
+}
+
+
 export {
+  createTag,
   getQueryIndex,
 };
--- tools/querybuilder/querybuilder.js
+++ tools/querybuilder/querybuilder.js
@@ -1,11 +1,122 @@
 // eslint-disable-next-line import/no-unresolved
 import DA_SDK from 'https://da.live/nx/utils/sdk.js';
-import { getQueryIndex } from '../../scripts/utilities.js';
+import {
+  createTag,
+  getQueryIndex
+} from '../../scripts/utilities.js';
+
+
+index.js
+import {$,jQuery} from 'jquery';
+// export for others scripts to use
+window.$ = $;
+window.jQuery = jQuery;
 
 export async function getProperties() {
-  const indexUrl = '/index.json';
-  const { data: allPages } = await getQueryIndex(indexUrl);
-  console.log(allPages);
+  const indexUrl = '/index-all.json';
+  const { columns: properties} = await getQueryIndex(indexUrl);
+  return properties;
+}
+
+export function addForm() {
+  var rules_basic = {
+    condition: 'AND',
+    rules: [{
+      id: 'price',
+      operator: 'less',
+      value: 10.25
+    }, {
+      condition: 'OR',
+      rules: [{
+        id: 'category',
+        operator: 'equal',
+        value: 2
+      }, {
+        id: 'category',
+        operator: 'equal',
+        value: 1
+      }]
+    }]
+  };
+  
+  $('#builder-basic').queryBuilder({
+    plugins: ['bt-tooltip-errors'],
+    
+    filters: [{
+      id: 'name',
+      label: 'Name',
+      type: 'string'
+    }, {
+      id: 'category',
+      label: 'Category',
+      type: 'integer',
+      input: 'select',
+      values: {
+        1: 'Books',
+        2: 'Movies',
+        3: 'Music',
+        4: 'Tools',
+        5: 'Goodies',
+        6: 'Clothes'
+      },
+      operators: ['equal', 'not_equal', 'in', 'not_in', 'is_null', 'is_not_null']
+    }, {
+      id: 'in_stock',
+      label: 'In stock',
+      type: 'integer',
+      input: 'radio',
+      values: {
+        1: 'Yes',
+        0: 'No'
+      },
+      operators: ['equal']
+    }, {
+      id: 'price',
+      label: 'Price',
+      type: 'double',
+      validation: {
+        min: 0,
+        step: 0.01
+      }
+    }, {
+      id: 'id',
+      label: 'Identifier',
+      type: 'string',
+      placeholder: '____-____-____',
+      operators: ['equal', 'not_equal'],
+      validation: {
+        format: /^.{4}-.{4}-.{4}$/
+      }
+    }],
+  
+    rules: rules_basic
+  });
+  
+  $('#btn-reset').on('click', function() {
+    $('#builder-basic').queryBuilder('reset');
+  });
+  
+  $('#btn-set').on('click', function() {
+    $('#builder-basic').queryBuilder('setRules', rules_basic);
+  });
+  
+  $('#btn-get').on('click', function() {
+    var result = $('#builder-basic').queryBuilder('getRules');
+    
+    if (!$.isEmptyObject(result)) {
+      alert(JSON.stringify(result, null, 2));
+    }
+  });
+}
+
+async function buildDropdownOptions() {
+  const propertyDropdown = document.querySelector('form#propertyCheck select#propertyName');
+  const properties = await getProperties();
+  console.log(properties);
+  properties.forEach((prop) => {
+    const option = createTag('option', { value: `${prop}` }, `${prop}`);
+    propertyDropdown.append(option);
+  });
 }
 
 function typeChange(e) {
@@ -81,7 +192,10 @@ function initControls(actions) {
 }
 
 (async function init() {
-  const { actions } = await DA_SDK;
+  console.log('init');
+  //const { actions } = await DA_SDK;
 
+  buildDropdownOptions();
+  addForm();
   initControls(actions);
 }());
